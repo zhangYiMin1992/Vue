e2abd4dcddbdee16905efd831f0da74d9f2c8bef
simple <470779536@qq.com>
2018-07-26 11:32:36 +0800

    小米商城首页导航

--- xiaomi/src/components/TopBar.vue
+++ xiaomi/src/components/TopBar.vue
@@ -3,16 +3,19 @@
     <div class="top-bar clearfix">
       <div class="topbar-nav">
         <ul class="nav-wrap">
-          <li class="nav"><a class="nav-name parting" href="JavaScript:;">小米</a></li>
-          <li class="nav"><a class="nav-name parting" href="JavaScript:;">小米</a></li>
-          <li class="nav"><a class="nav-name parting" href="JavaScript:;">小米</a></li>
-          <li class="nav"><a class="nav-name parting" href="JavaScript:;">小米</a></li>
-          <li class="nav"><a class="nav-name" href="JavaScript:;">小米</a></li>
+          <li class="nav" v-for="(item,index) in navs" :key="item.name">
+            <template v-if="item.name === '小米商城'">
+              <a class="nav-name" :class="{'parting':index!==8}" :href="item.sourceUrl">{{item.name}}</a>
+            </template>  
+            <template v-else>
+              <a class="nav-name" :class="{'parting':index!==8}" :href="item.sourceUrl" target="_blank">{{item.name}}</a>
+            </template>
+          </li>
         </ul>
       </div>
       <div class="topbar-info">
-        <a href="JavaScript:;" class="parting">登录</a>
-        <a href="JavaScript:;">注册</a>
+        <a href="http://order.mi.com/site/login?redirectUrl=http://www.mi.com/index.html" class="parting">登录</a>
+        <a href="https://account.xiaomi.com/pass/register">注册</a>
       </div>
       <div class="topbar-cart">
         <a href="javascript:;"><icon name="cart-plus"></icon>购物车（<span>0</span>）</a>
@@ -33,8 +36,30 @@ export default {
   name: 'TopBar',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      navs: [
+				{name: '小米商城', sourceUrl: 'http://www.mi.com/index.html'},
+				{name: 'MIUI', sourceUrl: 'http://www.miui.com/'},
+				{name: '米聊', sourceUrl: 'http://www.miliao.com/'},
+				{name: '游戏', sourceUrl: 'http://game.xiaomi.com/'},
+				{name: '多看阅读', sourceUrl: 'http://www.duokan.com/'},
+				{name: '云服务', sourceUrl: 'https://i.mi.com/'},
+				{name: '小米网移动版', sourceUrl: 'http://www.mi.com/c/appdownload/'},
+				{name: '问题反馈', sourceUrl: 'http://static.mi.com/feedback/'},
+				{name: 'Select Region', sourceUrl: 'http://www.mi.com/index.html'}
+			],
+			timer: '',
+			cartStatus: false
     }
+  },
+  mounted(){
+    this.$nextTick( () =>{
+      
+    })
+  },
+  methods:{
+      init(){
+
+      }
   }
 }
 </script>
